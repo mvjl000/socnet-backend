10e93d4061f51b42e4ed4374d22b1747de02ba82
mvjl000 <milosz.devmail@gmail.com>
2021-02-22 22:40:30 +0100

    Create CORS headers middleware

--- server.js
+++ server.js
@@ -10,14 +10,25 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
+
+app.use('/api/user', userRoutes);
+
 app.use((error, req, res, next) => {
   res.status(error.code || 500);
   res.json({
     message: error.message || 'Unexpected server-side error occured.',
   });
 });
-
-app.use('/api/user', userRoutes);
 
 mongoose
   .connect(
