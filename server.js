6686f4f90028d5055620fc8ab57ec988a2414e8f
mvjl000 <milosz.devmail@gmail.com>
2021-02-22 17:26:59 +0100

    Add error handling middleware.

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const userRoutes = require('./routes/user-routes');
 
@@ -7,6 +10,20 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((error, req, res, next) => {
+  res.status(error.code || 500);
+  res.json({
+    message: error.message || 'Unexpected server-side error occured.',
+  });
+});
+
 app.use('/api/user', userRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(
+    `mongodb+srv://mvjl000:${process.env.DB_PASSWORD}@socnet-db.2iozq.mongodb.net/socnetdb?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
