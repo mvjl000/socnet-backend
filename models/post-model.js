1d02b25f35d2d8ef96e1b710b3569996f45f9e54
mvjl000 <milosz.devmail@gmail.com>
2021-03-08 23:52:59 +0100

    Edit post schema

--- models/post-model.js
+++ models/post-model.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
+  creator: { type: String, required: true },
 });
 // creator: { type: mongoose.Types.ObjectId, required: true, ref: 'User' },
 
