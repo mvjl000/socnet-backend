e0dc23a95f63a9d79c2310e07f660539bbe9bbbd
mvjl000 <milosz.devmail@gmail.com>
2021-03-08 17:08:00 +0100

    Middleware for unknown routes

--- server.js
+++ server.js
@@ -25,6 +25,11 @@ app.use((req, res, next) => {
 app.use('/api/user', userRoutes);
 app.use('/api/posts', postRoutes);
 
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
+});
+
 app.use((error, req, res, next) => {
   res.status(error.code || 500);
   res.json({
