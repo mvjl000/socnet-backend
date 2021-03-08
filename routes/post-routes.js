31adbe2411c35ae6f0faf3a3130ff44062e15886
mvjl000 <milosz.devmail@gmail.com>
2021-03-08 17:19:44 +0100

    Auth middleware

--- routes/post-routes.js
+++ routes/post-routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 // const { check } = require('express-validator');
 
+const checkAuth = require('../middleware/check-auth');
 const postControllers = require('../controllers/post-controllers');
 
 const router = express.Router();
+
+router.use(checkAuth);
 
 router.post('/createPost', postControllers.createPost);
 
