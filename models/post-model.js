7fafb911bcc8539137a33c8d4934ad443ebb0822
mvjl000 <milosz.devmail@gmail.com>
2021-03-12 00:38:24 +0100

    Change post model

--- models/post-model.js
+++ models/post-model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-  title: { type: String, required: true },
+  title: { type: String, required: false },
   content: { type: String, required: true },
   creator: { type: String, required: true },
 });
