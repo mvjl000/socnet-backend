e852e02d2c4626642d73f66b34f4e4fe1defdb05
mvjl000 <milosz.devmail@gmail.com>
2021-03-16 22:02:27 +0100

    Authorization to delete post request

--- controllers/post-controllers.js
+++ controllers/post-controllers.js
@@ -35,11 +35,12 @@ exports.getUserPosts = async (req, res, next) => {
 };
 
 exports.createPost = async (req, res, next) => {
-  const { title, content, creator } = req.body;
+  const { title, content, creatorId, creator } = req.body;
 
   const createdPost = new Post({
     title,
     content,
+    creatorId,
     creator,
   });
 
@@ -71,15 +72,24 @@ exports.deletePost = async (req, res, next) => {
     return next(error);
   }
 
-  try {
-    await post.remove();
-  } catch (err) {
+  if (post.creatorId !== req.userData.userId) {
     const error = new HttpError(
-      'Could not delete post due to server error. Please try again later.',
-      500
+      'You are not allowed to delete this post.',
+      401
     );
     return next(error);
   }
+
+  if (post)
+    try {
+      await post.remove();
+    } catch (err) {
+      const error = new HttpError(
+        'Could not delete post due to server error. Please try again later.',
+        500
+      );
+      return next(error);
+    }
 
   res.json({ message: 'Post deleted succesfully' });
 };
--- models/post-model.js
+++ models/post-model.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema({
   title: { type: String, required: false },
   content: { type: String, required: true },
+  creatorId: { type: String, required: true },
   creator: { type: String, required: true },
 });
 // creator: { type: mongoose.Types.ObjectId, required: true, ref: 'User' },
