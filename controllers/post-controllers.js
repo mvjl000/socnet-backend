878bee79f0e1efaa7b4b0f5c5158cfe518e9eb42
mvjl000 <milosz.devmail@gmail.com>
2021-03-31 00:20:24 +0200

    Handle action type DISLIKE

--- controllers/post-controllers.js
+++ controllers/post-controllers.js
@@ -223,6 +223,23 @@ exports.likeAction = async (req, res, next) => {
     }
   } else if (action_type === 'DISLIKE') {
     post.likesCount--;
+    const updatedLikedPosts = user.likedPosts.filter(
+      (post) => post._id.toString() !== postId
+    );
+    try {
+      const sess = await mongoose.startSession();
+      sess.startTransaction();
+      await post.save({ session: sess });
+      user.likedPosts = updatedLikedPosts;
+      await user.save({ session: sess });
+      await sess.commitTransaction();
+    } catch (err) {
+      const error = new HttpError(
+        'Something went wrong, could not save post likes value.',
+        500
+      );
+      return next(error);
+    }
   }
 
   res.json({
